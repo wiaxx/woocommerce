ec550e642f9cd58cb3c0b0f37c3a6ec96be9336e
Veronica Torell <veronicasofie@hotmail.com>
2022-09-27 16:13:24 +0200

    last changes before merge

--- themes/shoppis/template-parts/listed-stores.php
+++ themes/shoppis/template-parts/listed-stores.php
@@ -1,7 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listed Stores</title>
+</head>
+<body>
+
 <div class="listed-stores">
     <?php the_title(); ?>
-    <?php the_field('store_address', get_the_id()); ?>
     <?php the_field('store_phone_number', get_the_id()); ?>
+    <?php the_field('store_address', get_the_id()); ?>
     <?php the_field('store_opening_hours', get_the_id()); ?>
-
 </div>
+    
+</body>
+</html>
