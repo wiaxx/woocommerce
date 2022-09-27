3615fbdfd38febd7c385caeb8bb7f9763531d3ee
wiaxx <amanda.wikstrom@hotmail.com>
2022-09-27 14:49:28 +0200

    create intro block

---
+++ themes/shoppis/template-parts/blocks/intro.php
@@ -0,0 +1,11 @@
+<div class="intro-block">
+
+    <h2 class="intro-title">
+        <?php the_field('intro_heading'); ?>
+    </h2>
+
+    <p class="intro-text">
+        <?php the_field('intro_text'); ?>
+    </p>
+
+</div>
