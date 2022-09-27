acc3a63142a2c32960a37704d15849fbd8cb4603
wiaxx <amanda.wikstrom@hotmail.com>
2022-09-27 14:33:27 +0200

    add header to every page

--- themes/shoppis/template-parts/nav.php
+++ themes/shoppis/template-parts/nav.php
@@ -1,11 +1,7 @@
-<?php
-
-$site_title = get_bloginfo('name');
-$site_url = network_site_url();
-?>
-
 <header>
+
     <?php
-    wp_nav_menu(array('theme-location' => 'main-menu'));
+    wp_nav_menu(array('theme_location' => 'main-menu'));
     ?>
+
 </header>
