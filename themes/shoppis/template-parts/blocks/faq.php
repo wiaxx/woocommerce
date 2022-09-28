177a6a03ded6e2e98fbda0c6ea8a86e72ce3e8b2
Veronica Torell <veronicasofie@hotmail.com>
2022-09-28 13:32:24 +0200

    Update faq-branch

--- themes/shoppis/template-parts/blocks/faq.php
+++ themes/shoppis/template-parts/blocks/faq.php
@@ -27,19 +27,3 @@
     <?php wp_reset_postdata(); ?>
 
     <?php endif; ?>
-
-            
-
-
-
-
-
-    <!-- <div class="faq-block-content">
-        <h1><?php the_field('heading')?></h1>
-    </div>
-</div> -->
-
-<!-- if($image) {
-    $image_id = $image['id'];
-    echo wp_get_attachment_image($image_id, 'large');
-} -->
--- themes/shoppis/template-parts/listed-stores.php
+++ themes/shoppis/template-parts/listed-stores.php
@@ -1,7 +1,7 @@
 <div class="listed-stores">
-    <?php the_title(); ?>
-    <?php the_field('store_address', get_the_id()); ?>
-    <?php the_field('store_phone_number', get_the_id()); ?>
-    <?php the_field('store_opening_hours', get_the_id()); ?>
+ <p class="store-title"><?php the_title(); ?> </p> 
+ <p class="store-phone"><?php the_field('store_phone_number', get_the_id()); ?></p> 
+ <p class="store-address"> <?php the_field('store_address', get_the_id()); ?></p> 
+  <p class="store-hours"><?php the_field('store_opening_hours', get_the_id()); ?> </p> 
 
 </div>
