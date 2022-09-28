41c4534f4a78d21ee8022d933fa16b190bc656ed
wiaxx <amanda.wikstrom@hotmail.com>
2022-09-28 11:44:18 +0200

    change custom field, input number. WIP styling for product highlight

--- themes/shoppis/template-parts/blocks/product-highlight.php
+++ themes/shoppis/template-parts/blocks/product-highlight.php
@@ -1,12 +1,11 @@
 <?php
-$product_id = 42;
+$product_id = get_field('product_id');
 ?>
 
 <div class="product-highlight-block">
 
     <?php
     echo do_shortcode('[product_page id=' . $product_id . ']');
-
     ?>
 
 </div>
