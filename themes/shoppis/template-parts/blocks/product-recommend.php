2b3de9e4b7e381bf44ede43ff5a507ae0acd155a
Veronica Torell <veronicasofie@hotmail.com>
2022-09-28 13:39:07 +0200

    Product recommend

---
+++ themes/shoppis/template-parts/blocks/product-recommend.php
@@ -0,0 +1,47 @@
+<div class="product-recommend-wrapper">
+
+    <h2 class="product-category">
+        <?php the_field('product_category'); ?>
+    </h2>
+
+    <h2 class="recommend-title">
+        <?php the_field('product_recommend_title'); ?>
+    </h2>
+
+    <p class="recommend-text">
+        <?php the_field('product_recommend_text'); ?>
+    </p>
+
+    <p class="recommend-img-1">
+        <?php the_field('product_recommend_img1'); ?>
+    </p>
+
+    <h1 class="recommend-img-title-1">
+        <?php the_field('image_title1'); ?>
+    </h1>
+
+    <p class="recommend-img-text-1">
+        <?php the_field('image_text1'); ?>
+    </p>
+
+    <p class="recommend-img-2">
+        <?php the_field('product_recommend_img2'); ?>
+    </p>
+
+    <p class="recommend-img-title-2">
+        <?php the_field('image_title2'); ?>
+    </p>
+
+    <p class="recommend-img-text-2">
+        <?php the_field('image_text2'); ?>
+    </p>
+
+    <p class="product-price1">
+        <?php the_field('product_price1'); ?>
+    </p>
+
+    <p class="product-price2">
+        <?php the_field('product_price2'); ?>
+    </p>
+
+</div>
