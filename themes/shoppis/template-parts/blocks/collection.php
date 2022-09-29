a4bd906cfe5ccc3a0158aab34234be3b239d7009
wiaxx <amanda.wikstrom@hotmail.com>
2022-09-29 14:44:31 +0200

    WIP styling and collection block

---
+++ themes/shoppis/template-parts/blocks/collection.php
@@ -0,0 +1,51 @@
+<!-- Collection Block -->
+<?php
+$categories  = get_terms('product_cat');
+$title = get_field('collections_heading');
+$description = get_field('collections_desc');
+?>
+
+<div class="collection-block">
+
+    <h2> <?php echo $title; ?> </h2>
+
+    <p> <?php echo $description; ?> </p>
+
+    <div class="collection-categories">
+        <?php
+        if ($categories) :
+            foreach ($categories as $category) {
+
+                $id = $category->term_id;
+                $thumbnail_id = get_term_meta($id, 'thumbnail_id', true);
+                $image = wp_get_attachment_image($thumbnail_id, 'medium');
+        ?>
+
+                <div class="collection-category">
+
+                    <div class="collection-img">
+                        <?php echo $image; ?>
+                    </div>
+
+                    <div class="collection-text">
+                        <h1>
+
+                            <?php echo $category->name ?>
+                        </h1>
+
+                        <p>
+                            <?php echo $category->description ?>
+                        </p>
+
+                        <a href="<?php echo get_term_link($category); ?>">
+                            View collection
+                        </a>
+                    </div>
+
+                </div>
+        <?php }
+        endif;
+        ?>
+
+    </div>
+</div>
