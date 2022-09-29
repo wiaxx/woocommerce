2392d1baa9b819fcde830c4ad2f588b431f950ba
Veronica Torell <veronicasofie@hotmail.com>
2022-09-29 17:50:30 +0200

    News showing on index

--- themes/shoppis/index.php
+++ themes/shoppis/index.php
@@ -1,1 +1,45 @@
 <?php get_header() ?>
+
+<?php /* Template Name: News */ ?>
+
+<?php 
+get_header();
+$site_title = get_bloginfo('name');
+$site_url = network_site_url('/');
+?>
+
+<h1 class="header-logo">
+<?php echo $site_title; ?>
+</h1>
+
+<h3 class="news-news"><?php the_title(); ?> </h3>  
+
+
+<?php
+$the_query = new WP_Query(array(
+    'posts_per_page' => 6,
+));
+?>
+
+<?php if ($the_query->have_posts()) : ?>
+    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+    
+
+      <h3 class="all-news-title"><?php the_title(); ?> </h3>  
+      <a class="permalink" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+
+      <span class="date"> <?php the_time(get_option('date_format')); ?> </span>
+
+      <div class="all-news-content">
+        
+           
+          </div>
+        <?php the_content(); ?> 
+      </div>
+
+
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+    <?php endif; ?>
+
+<?php get_footer(); ?>
---
+++ themes/shoppis/page-templates/single-news.php
@@ -0,0 +1,27 @@
+ <!-- <?php /* Template Name: Single News */ ?> 
+
+
+<?php
+$the_query = new WP_Query(array(
+    'posts_per_page' => 1,
+));
+?>
+
+<?php if ($the_query->have_posts()) : ?>
+    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+     
+
+      <h3 class="single-news-title"><?php the_title(); ?> </h3>  
+      <span class="date"> <?php the_time(get_option('date_format')); ?> </span>
+      <div class="single-news-content">
+        <?php the_content(); ?>
+      </div>
+
+
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+    <?php endif; ?>
+
+<a href="./page-news.php">Go back to News</a>
+
+<?php get_footer(); ?> -->
