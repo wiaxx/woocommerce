5b736fd55c73bc8642f245dfbd2cfab7284b7508
Veronica Torell <veronicasofie@hotmail.com>
2022-10-03 13:45:30 +0200

    comit before merge

--- themes/shoppis/footer.php
+++ themes/shoppis/footer.php
@@ -1,28 +1,4 @@
-<!-- 
 <div class="footer-wrap">
-
-<h3 class="footer-nav">Navigate</h3>
-<div class="nav-links">
-<a href="/">Shop</a>
-<a href="/about-us">Om oss</a>
-<a href="/stores">Våra butiker</a>
-<a href="/my-account">Logga in</a>
-</div>
-
-<h3 class="footer-info">Information</h3>
-<div class="nav-links-2"> 
-<a href="/privacy-policy">Privacy Policy</a>
-<a href="/terms">Terms & Agreement</a>
-<h2 class="logo">SHOPPIS</h2>
-</div>
-
-</div>
-
-
- -->
-
-
- <div class="footer-wrap">
 
  <h3 class="footer-nav">Navigate</h3>
  <div class="nav-links">
