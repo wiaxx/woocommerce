547443d8478061a4e40f4682d7885c8087e80785
Veronica Torell <veronicasofie@hotmail.com>
2022-10-04 15:25:41 +0200

    Trying so show product img

--- themes/shoppis/template-parts/blocks/product-recommend.php
+++ themes/shoppis/template-parts/blocks/product-recommend.php
@@ -1,23 +1,31 @@
     <div class="products-recommend">
-<?php 
 
-// For categories 
+<!-- // For categories  -->
+<?php 
     $categories = get_field('categories');
      $products = get_field('products');
+?>
 
+
+
+
+
+
+<?php
      if ($categories) {
         foreach ($categories as $cat_id) {
 
-     if( $term = get_term_by( 'id', $cat_id, 'product_cat' ) ){
-      echo $term->name;
+     if( $products = get_term_by( 'id', $cat_id, 'product_cat' ) ){ ?>  
+    <div class="cat-name"> <?php echo $products->name; ?> </div>
+   
 
 
+   <?php
     $thumbnail_id = get_term_meta( $cat_id, 'thumbnail_id', true ); 
 
     $image = wp_get_attachment_url( $thumbnail_id ); 
 
     echo "<img src='{$image}' alt='' width='200' height='200' />";
-    
     
  }
 
@@ -26,34 +34,3 @@
     }
  ?>
  </div>
-
-
- <!-- For products (doesn't work)  -->
- <!-- <?php 
-
-    $categories = get_field('categories');
-     $products = get_field('products');
-
-     if ($categories) {
-        foreach ($categories as $cat_id) {
-
-     if( $term = get_term_by( 'id', $cat_id, 'product_cat' ) ){
-      echo $term->name;
-
-
-    $thumbnail_id = get_term_meta( $cat_id, 'thumbnail_id', true ); 
-
-    $image = wp_get_attachment_url( $thumbnail_id ); 
-
-    echo "<img src='{$image}'";
-}
-
-}  
-  
-}
-?>     -->
-   
-
-
-
-    
