49da62bf5f0fa062b7078f1265b884560a9dee49
Veronica Torell <veronicasofie@hotmail.com>
2022-10-04 17:22:25 +0200

    styling

---
+++ themes/shoppis/search.php
@@ -0,0 +1,35 @@
+<?php  /* Template Name: Search Form */ ?>
+<?php get_header() ?>
+
+<div class="search-title"> <?php the_title(); ?> </div>
+<div class="search-container">
+    <div class="search-form">
+    <?php get_search_form(); ?>
+    </div>
+
+    <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <div class="search-result">
+            <p>
+             <a href="<?php the_permalink(); ?>">
+             <?php the_post_thumbnail('thumbnail') ?>
+             </a> 
+            </p>
+
+                <span><?php the_excerpt(); ?></span>
+             </div>
+
+        <?php endwhile; ?>
+        <?php else : ?>
+
+
+        <div class="search-result">
+        <h2> <?php _e('No items found'); ?></h2>
+        </div>
+
+     <?php endif; ?>
+</div>
+
+<?php
+get_footer();
+?>
