f2f8890ca0d8e64daa6589e203308786f84507a6
wiaxx <amanda.wikstrom@hotmail.com>
2022-10-05 12:19:08 +0200

    style product recommend and fix content in product-recommend block

--- themes/shoppis/template-parts/blocks/product-recommend.php
+++ themes/shoppis/template-parts/blocks/product-recommend.php
@@ -3,51 +3,42 @@
 $title = get_field('product_recommend_title');
 $desc = get_field('product_recommend_text');
 $products = get_field('products');
-
-echo '<pre>';
-var_dump($products);
-echo '</pre>';
 ?>
 
 <div class="products-recommend">
 
-    <h1 class="recommend-title">
-        <?php echo $title; ?>
-    </h1>
+    <h1 class="recommend-title"> <?php echo $title; ?> </h1>
 
-    <p class="recommend-text">
-        <?php echo $desc; ?>
-    </p>
+    <p class="recommend-text"> <?php echo $desc; ?> </p>
 
-    <?php if ($products) :
-        foreach ($products as $product) { ?>
+    <div class="products-wrapper">
 
-            <div class="product-recommend">
+        <?php if ($products) :
+            foreach ($products as $product) {
+                $wc_product = wc_get_product($product->ID);
+        ?>
+                <div class="product-recommend">
 
-                <div class="recommend-img">
-                    <?php echo wp_get_attachment_image($product->ID, 'large'); ?>
+                    <div class="recommend-img">
+                        <a href="<?php echo get_permalink($wc_product->id); ?>">
+                            <?php echo wp_get_attachment_image($wc_product->image_id, 'large'); ?>
+                        </a>
+                    </div>
+
+                    <div class="recommend-info">
+                        <h2> <?php echo $wc_product->name; ?> </h2>
+
+                        <p> <?php echo $wc_product->short_description ?> </p>
+
+                        <p class="recommend-price">
+                            <?php echo $wc_product->price; ?> SEK
+                        </p>
+                    </div>
+
                 </div>
 
-                <div class="recommend-info">
-                    <h2>
-                        <?php echo $product->post_title ?>
-                    </h2>
-
-                    <p>
-                        <?php echo $product->post_excerpt ?>
-                    </p>
-
-                    <!-- <a href="<?php // echo get_term_link($category); ?>">
-                        View collection
-                    </a> -->
-                </div>
-
-            </div>
-
-    <?php }
-    endif;
-
-
-
-
-    ?>
+        <?php }
+        endif;
+        ?>
+    </div>
+</div>
