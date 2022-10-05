b9b822e6b0f56ec3672915a25839717dca28f3fb
wiaxx <amanda.wikstrom@hotmail.com>
2022-10-05 09:30:31 +0200

    add template-part and create half-img-text single product page

--- themes/shoppis/woocommerce.php
+++ themes/shoppis/woocommerce.php
@@ -4,24 +4,15 @@ get_header();
 
 <div class="woocommerce">
 
-    <?php
-    woocommerce_content();
-    ?>
+    <?php woocommerce_content(); ?>
 
-    <?php
-    if (is_product_category()) {
-    ?>
+    <?php if (is_product_category()) : ?>
         <?php get_template_part('template-parts/two-categories'); ?>
-    <?php
-    } ?>
+    <?php endif; ?>
 
-    <?php
-    if (is_product()) {
-    ?>
-    <h1>hejsan</h1>
-        <?php // get_template_part('template-parts/two-categories'); ?>
-    <?php
-    } ?>
+    <?php if (is_product()) : ?>
+        <?php get_template_part('template-parts/half-img-text'); ?>
+    <?php endif; ?>
 
 </div>
 
